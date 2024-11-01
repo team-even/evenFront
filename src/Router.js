e5f6cba4d02664a67da861be7f6f4b40e241e8ea
김아영 <thay123028@naver.com>
2024-11-01 21:19:46 +0900

    Initial commit

---
+++ src/Router.js
@@ -0,0 +1,9 @@
+import { Route, Routes } from "react-router-dom"
+import { HomePage } from "./pages/HomePage"
+export default function Router() {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />}/>
+    </Routes>
+  )
+}
