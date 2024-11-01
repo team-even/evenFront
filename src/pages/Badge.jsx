66c2830365c4c79090183edf3b833c591868bde5
김아영 <thay123028@naver.com>
2024-11-02 08:34:17 +0900

    마이페이지 뱃지 줄바꿈 수정

--- src/pages/Badge.jsx
+++ src/pages/Badge.jsx
@@ -5,11 +5,11 @@ import BackBtn from "../components/BackBtn";
 
 const dummyBadges = [
   { name: "채식 식단 3번 초보자", icon: "🌱" },
-  { name: "전통시장 누적 3회 방문", icon: "🛒" },
-  { name: "다회용기 누적 3회 포장", icon: "♻️" },
-  { name: "다회용기 누적 5회 열심히!", icon: "♻️" },
-  { name: "부산로컬음식 누적 3회 방문", icon: "🍲" },
-  { name: "친환경 용기 누적 3회 구매", icon: "🌍" },
+  { name: "전통시장 누적 <br/>3회 방문", icon: "🛒" },
+  { name: "다회용기 누적 <br/>3회 포장", icon: "♻️" },
+  { name: "다회용기 누적 <br/>5회 열심히!", icon: "♻️" },
+  { name: "부산로컬음식 <br/>누적 3회 방문", icon: "🍲" },
+  { name: "친환경 용기 <br/>누적 3회 구매", icon: "🌍" },
   { name: "다회용기 누적 10회 사용", icon: "♻️" }
 ]
 function Badge() {
